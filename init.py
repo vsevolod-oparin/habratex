6ee8d722c12afb2fe0a253335bbe1c8db9f9a0c2
Vsevolod Oparin <oparin.vsevolod@gmail.com>
2015-07-25 18:10:03 +0300

    Soft link added

--- init.py
+++ init.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
 
 default = "default.json"
+poster  = "poster.py"
+link    = "/usr/bin/poster"
+
 place = "__DEFAULT_SETTINGS__"
 path = os.path.abspath(default).replace("\\", "\\\\")
 with open("poster.py", "r") as f:
@@ -10,4 +14,6 @@ with open("poster.py", "r") as f:
 code = code.replace(place, "\"{0}\"".format(path))
 with open("poster.py", "w") as f:
     f.write(code)
+
+subprocess.call(["ln", "-s", os.path.abspath(poster), link])
 
